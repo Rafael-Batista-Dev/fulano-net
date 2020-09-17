4604ed525352dc75777bdf1ca04a72a966666b20
unknown <rafaelbatista.sth@gmail.com>
2020-09-17 19:22:45 -0300

    implement react-image-gallery

--- src/pages/components/Slide/index.js
+++ src/pages/components/Slide/index.js
@@ -1,15 +1,26 @@
 import React from "react";
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from "react-image-gallery";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
 
 const Slide = () => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <h3>Slide</h3>
-          </div>
-        </div>
-      </div>
+      <ImageGallery items={images} />;
     </>
   );
 };
