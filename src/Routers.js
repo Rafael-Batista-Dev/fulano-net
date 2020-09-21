401c2f42452a7a9f4cb2a4fae1d47cea42c17918
Rafael Batista <dev.rafaelbatista@gmail.com>
2020-09-21 14:51:36 +0000

    route adjustments

--- src/Routers.js
+++ src/Routers.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import Index from "./pages";
+import Index from "./pages/Index";
 import Navbar from "./pages/components/Navbar";
 import Footer from "./pages/components/Footer";
 import Internet from "./pages/Internet";
