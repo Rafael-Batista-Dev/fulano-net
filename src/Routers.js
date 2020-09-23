f961f06295971b50708b5f86eacf262d8db9dada
unknown <rafaelbatista.sth@gmail.com>
2020-09-23 18:00:49 -0300

    improving the login form

--- src/Routers.js
+++ src/Routers.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import Index from "./pages/Index";
+import Index from "./pages";
 import Navbar from "./pages/components/Navbar";
 import Footer from "./pages/components/Footer";
 import Contact from "./pages/components/Contact";
--- src/pages/components/Login/index.js
+++ src/pages/components/Login/index.js
@@ -5,92 +5,73 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <>
-      <div
-        className="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto"
-        style={{ marginTop: "3rem", width: "30%" }}
-      >
-        <div className="card card0 border-0">
-          <div className="row d-flex">
-
-            <div className="col-lg-12">
-              <div className="card2 card border-0 px-4 py-5">
-                <center>
-                    <img
-                    src={Logo}
-                    className="logo"
-                    alt="Brisanet"
-                  />
-                </center>
-                <br />
-                <div className="row mb-3 ">
-                  <div className="facebook text-center mr-1">
-                    <div className="fa fa-facebook"> Facebook</div>
-                  </div>
-                  <div className="gmail text-center mr-1">
-                    <div className="fa fa-google"> Gmail</div>
-                  </div>
-                  <div className="linkedin text-center mr-1">
-                    <div className="fa fa-linkedin"> Linkedin</div>
-                  </div>
+      <div className="container-fluid " style={{ marginTop: "7rem" }}>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="card main-div">
+              <center>
+                <img src={Logo} className="logo" alt="Brisanet" />
+              </center>
+              <br />
+              <div className="btn-group btn-group-toggle">
+                <div className="facebook text-center ">
+                  <div className="fa fa-facebook"> Facebook</div>
                 </div>
-                <div className="row px-3 mb-2">
-                  <div className="line"></div>{" "}
-                  <small className="or text-center">OU</small>
-                  <div className="line"></div>
+                <div className="gmail text-center ">
+                  <div className="fa fa-google"> Gmail</div>
                 </div>
-                <div className="row px-3">
-                  <label className="mb-2">
-                    <h6 className="mb-0 text-sm">Seu Email</h6>
-                  </label>
+              </div>
+              <div className="row px-3 mt-4 mb-4">
+                <div className="line"></div>{" "}
+                <small className="or text-center">OU</small>
+                <div className="line"></div>
+              </div>
+              <div className="row px-3">
+                <label className="mb-2">
+                  <h6 className="mb-0 text-sm">Seu Email</h6>
+                </label>
+                <input
+                  className="mb-2"
+                  type="text"
+                  name="email"
+                  placeholder="Informe seu endereço de email"
+                ></input>
+              </div>
+              <div className="row px-3">
+                <label className="mb-2">
+                  <h6 className="mb-0 text-sm">Senha</h6>
+                </label>
+                <input
+                  className="mb-2"
+                  type="password"
+                  name="email"
+                  placeholder="Informe sua senha"
+                ></input>
+              </div>
+              <div className="row px-3 mt-2  mb-2">
+                <div className="custom-control custom-checkbox custom-control-inline">
                   <input
-                    className="mb-2"
-                    type="text"
-                    name="email"
-                    placeholder="Informe seu endereço de email"
+                    id="chk1"
+                    type="checkbox"
+                    name="chk"
+                    className="custom-control-input"
                   ></input>
-                </div>
-                <div className="row px-3">
-                  <label className="mb-2">
-                    <h6 className="mb-0 text-sm">Senha</h6>
+                  <label className="custom-control-label text-sm">
+                    Memorizar
                   </label>
-                  <input
-                    className="mb-2"
-                    type="password"
-                    name="email"
-                    placeholder="Informe sua senha"
-                  ></input>
                 </div>
-                <div className="row px-3 mb-2">
-                  <div className="custom-control custom-checkbox custom-control-inline">
-                    <input
-                      id="chk1"
-                      type="checkbox"
-                      name="chk"
-                      className="custom-control-input"
-                    ></input>
-                    <label className="custom-control-label text-sm">
-                      Memorizar
-                    </label>
-                  </div>
-                  <Link to="/" className="ml-auto mb-0 text-sm">
-                    Recuperar senha
-                  </Link>
-                </div>
-                <div className="row mb-3 px-3">
-                  {" "}
-                  <button
-                    type="submit"
-                    className="btn btn_get btn-lg btn-block btn_get_two"
-                  >
-                    Entrar
-                  </button>{" "}
-                </div>
-                <div className="row mb-4 px-3">
-                  {" "}
-                  <small className="font-weight-bold">
-                    Não tem conta? <a className="text-danger ">Registre-se</a>
-                  </small>{" "}
-                </div>
+                <Link to="/" className="ml-auto text-sm">
+                  Recuperar senha
+                </Link>
+              </div>
+              <div className="row px-3">
+                {" "}
+                <button
+                  type="submit"
+                  className="btn btn_get btn-lg btn-block btn_get_two"
+                >
+                  Entrar
+                </button>{" "}
               </div>
             </div>
           </div>
