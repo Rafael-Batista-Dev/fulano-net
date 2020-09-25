a42d234976bbd6c3fce1374191fa38d861e7b57f
unknown <rafaelbatista.sth@gmail.com>
2020-09-25 17:12:06 -0300

    implement chat

--- src/pages/components/Contact/index.js
+++ src/pages/components/Contact/index.js
@@ -6,12 +6,10 @@ const Contact = () => {
       <div className="container">
         <div className="row">
           <div className="nb-form">
+            <div className="user-icon">
+              <i class="fa fa-comment"></i>
+            </div>
             <p className="title-contact">Fale com um especialista</p>
-            <img
-              src="https://lh3.googleusercontent.com/-LvTWzTOL4c0/V2yhfueroyI/AAAAAAAAGZM/Ebwt4EO4YlIc03tw8wVsGrgoOFGgAsu4wCEw/w140-h140-p/43bf8578-86b8-4c1c-86a6-a556af8fba13"
-              alt=""
-              className="user-icon"
-            />
             <form>
               <input type="text" name="cpname" placeholder="Nome:" required />
               <input
