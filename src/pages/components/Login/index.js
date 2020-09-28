c7d98b81aaf58634a35dba05af107f7430802034
unknown <rafaelbatista.sth@gmail.com>
2020-09-28 20:27:39 -0300

    implement those in the login component

--- src/pages/components/Login/index.js
+++ src/pages/components/Login/index.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Logo from "../../../assets/logo1.ico";
 import { Link } from "react-router-dom";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Login = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <>
       <div className="container-fluid " style={{ marginTop: "7rem" }}>
         <div className="row">
           <div className="col-lg-12">
-            <div className="card main-div">
+            <div className="card main-div" data-aos="fade-down">
               <center>
                 <img src={Logo} className="logo" alt="Brisanet" />
               </center>
