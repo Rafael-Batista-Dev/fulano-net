a30957ba1c73132f367c41a757ce4ae0aa1947bb
unknown <rafaelbatista.sth@gmail.com>
2020-09-28 20:23:51 -0300

    remove banners

--- src/pages/components/Login/index.js
+++ src/pages/components/Login/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../../assets/logo.svg";
+import Logo from "../../../assets/logo1.ico";
 import { Link } from "react-router-dom";
 
 const Login = () => {
