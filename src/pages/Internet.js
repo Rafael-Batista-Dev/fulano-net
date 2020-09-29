868ec29d07665fea793c87acfb8a2ca5008344a2
unknown <rafaelbatista.sth@gmail.com>
2020-09-29 17:25:57 -0300

    created page internet

--- src/pages/Internet.js
+++ src/pages/Internet.js
@@ -1,23 +1,96 @@
 import React from "react";
-import Plans from "./components/Plans";
-import bannerInternet from "../assets/banner-internet.jpg";
+import { Icon } from "react-icons-kit";
+import { ic_done_all } from "react-icons-kit/md/ic_done_all";
 
 export default function Internet() {
   return (
     <>
-      <div className="content">
-        <div style={{ marginTop: "5rem" }}>
-          <img style={{ width: "100%" }} src={bannerInternet} alt="mobile" />
-        </div>
-      </div>
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="content-title">
-              <h1>Melhores opções de planos</h1>
+              <h1>Internet para todos os gostos</h1>
             </div>
           </div>
-          <Plans />
+          <div className="col-lg-6">
+            <h3>
+              <strong>Melhores opções de planos</strong>
+            </h3>
+            <div className="content-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+              eius, accusamus corrupti laboriosam harum quaerat alias quos
+              incidunt a, debitis nam officiis laudantium consectetur
+              repudiandae tempore. Culpa repellat cumque nihil.
+            </div>
+            <div className="list">
+              <ul>
+                <li>
+                  <Icon className="checked-list" icon={ic_done_all} /> Teste de
+                  velocidade
+                </li>
+                <li>
+                  <Icon className="checked-list" icon={ic_done_all} /> Teste de
+                  velocidade
+                </li>
+                <li>
+                  <Icon className="checked-list" icon={ic_done_all} /> Teste de
+                  velocidade
+                </li>
+                <li>
+                  <Icon className="checked-list" icon={ic_done_all} /> Teste de
+                  velocidade
+                </li>
+                <li>
+                  <Icon className="checked-list" icon={ic_done_all} /> Teste de
+                  velocidade
+                </li>
+              </ul>
+            </div>
+            <button
+              className="btn btn_get btn_get_two mt4 float-left"
+              style={{ marginTop: "4rem" }}
+            >
+              Saiba mais <i className="fa fa-arrow-right"></i>
+            </button>
+          </div>
+          <div className="col-lg-3">
+            <div className="card-up">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                quis sapiente et, est at tenetur neque? Ducimus ea accusantium
+                eaque illo. Nam expedita alias ratione assumenda a temporibus
+                tenetur iste?
+              </p>
+            </div>
+
+            <div className="card-up">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                quis sapiente et, est at tenetur neque? Ducimus ea accusantium
+                eaque illo. Nam expedita alias ratione assumenda a temporibus
+                tenetur iste?
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-3" style={{ marginTop: "6rem" }}>
+            <div className="card-down">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                quis sapiente et, est at tenetur neque? Ducimus ea accusantium
+                eaque illo. Nam expedita alias ratione assumenda a temporibus
+                tenetur iste?
+              </p>
+            </div>
+
+            <div className="card-down">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                quis sapiente et, est at tenetur neque? Ducimus ea accusantium
+                eaque illo. Nam expedita alias ratione assumenda a temporibus
+                tenetur iste?
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
