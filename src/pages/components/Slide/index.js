15f1bca25d0d26fa8c2833177463bb2a4edb0821
unknown <rafaelbatista.sth@gmail.com>
2020-10-08 22:34:50 -0300

    created the div structure

--- src/pages/components/Slide/index.js
+++ src/pages/components/Slide/index.js
@@ -1,32 +1,18 @@
 import React from "react";
-import "react-image-gallery/styles/css/image-gallery.css";
-import ImageGallery from "react-image-gallery";
-
-const images = [
-  /*
-  
-  {
-    original:
-      "https://www.brisanet.com.br/assets/brisa/brisanet-telecomunicacoes.jpg",
-    thumbnail: "https:",
-  },
-  {
-    original:
-      "https://brisanetcampinacom.000webhostapp.com/site/img/intro-bg/02.jpg",
-    thumbnail: "https:",
-  },
-  */
-  {
-    original:
-      "https://www.jotaftelecom.com.br/wp-content/uploads/2019/04/banner3.png",
-    thumbnail: "https:",
-  },
-];
 
 const Slide = () => {
   return (
     <div style={{ marginTop: "5rem" }}>
-      <ImageGallery items={images} autoPlay={true} showThumbnails={false} />;
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12" style={{background: "black", height: "350px"}}></div>         
+            <div className="col-lg-4" style={{background: "red", height: "200px"}}></div>         
+            <div className="col-lg-4" style={{background: "black", height: "200px"}}></div>         
+            <div className="col-lg-4" style={{background: "red", height: "200px"}}></div>         
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
