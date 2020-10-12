8940e06c3d711bab2602db849d8554fadfb2cd02
unknown <rafaelbatista.sth@gmail.com>
2020-10-12 12:22:30 -0300

    banner01 created

--- src/pages/components/Slide/index.js
+++ src/pages/components/Slide/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Banner01 from '../../../assets/banner-01.jpg'
 
 const Slide = () => {
   return (
@@ -6,7 +7,11 @@ const Slide = () => {
       <div className="jumbotron jumbotron-fluid">
         <div className="container">
           <div className="row">
-            <div className="col-lg-12" style={{background: "black", height: "350px"}}></div>         
+            <div className="col-lg-12">
+
+                <img className='banner01' style={{width:'100%', marginBottom: '20px'}} src={Banner01} alt="net"/>
+      
+            </div>         
             <div className="col-lg-4" style={{background: "red", height: "200px"}}></div>         
             <div className="col-lg-4" style={{background: "black", height: "200px"}}></div>         
             <div className="col-lg-4" style={{background: "red", height: "200px"}}></div>         
