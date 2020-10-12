770b6f02ad3034e16b88758e8eb0a8f817925097
unknown <rafaelbatista.sth@gmail.com>
2020-10-12 12:56:24 -0300

    added new banner

--- src/pages/components/Slide/index.js
+++ src/pages/components/Slide/index.js
@@ -1,5 +1,8 @@
 import React from "react";
-import Banner01 from '../../../assets/banner-01.jpg'
+import Banner01 from '../../../assets/banner01.jpg'
+import Banner02 from '../../../assets/banner02.jpg'
+import Banner03 from '../../../assets/banner03.jpg'
+import Banner04 from '../../../assets/banner04.jpg'
 
 const Slide = () => {
   return (
@@ -12,9 +15,18 @@ const Slide = () => {
                 <img className='banner01' style={{width:'100%', marginBottom: '20px'}} src={Banner01} alt="net"/>
       
             </div>         
-            <div className="col-lg-4" style={{background: "red", height: "200px"}}></div>         
-            <div className="col-lg-4" style={{background: "black", height: "200px"}}></div>         
-            <div className="col-lg-4" style={{background: "red", height: "200px"}}></div>         
+            <div className="col-lg-4">
+                <img className='banner02' style={{width:'100%', marginBottom: '10px'}} src={Banner02} alt="net"/>
+
+            </div>         
+            <div className="col-lg-4">
+            <img className='banner03' style={{width:'100%', marginBottom: '10px'}} src={Banner03} alt="net"/>
+
+            </div>         
+            <div className="col-lg-4">
+                <img className='banner04' style={{width:'100%', marginBottom: '10px'}} src={Banner04} alt="net"/>
+
+            </div>         
           </div>
         </div>
       </div>
